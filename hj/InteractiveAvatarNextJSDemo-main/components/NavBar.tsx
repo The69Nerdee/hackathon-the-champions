e2627f47523c47636fc182c4317eda58765dbbf7
The69Nerdee <abbassi20290@gmail.com>
2025-02-02 10:39:03 +0100

    updates

---
+++ hj/InteractiveAvatarNextJSDemo-main/components/NavBar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
+import { GithubIcon, HeyGenLogo } from "./Icons";
+import { ThemeSwitch } from "./ThemeSwitch";
+
+export default function NavBar() {
+  return (
+    <Navbar className="w-full">
+      <NavbarBrand>
+        <Link isExternal aria-label="HeyGen" href="https://app.heygen.com/">
+          <HeyGenLogo />
+        </Link>
+        <div className="bg-gradient-to-br from-sky-300 to-indigo-500 bg-clip-text ml-4">
+          <p className="text-xl font-semibold text-transparent">
+            The champions
+          </p>
+        </div>
+      </NavbarBrand>
+      
+    </Navbar>
+  );
+}
